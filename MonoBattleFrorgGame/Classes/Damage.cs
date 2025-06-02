5e2dc004de9897bd4786975f42e589c8dc8962a0
ThatCubicGuy <vlasinalexcatalin@gmail.com>
2025-06-02 23:49:13 +0300

    Updated Clone() method to deep copy, made DamageProperties members accessible, created the start of something potentially great (DamageModifier)

--- MonoBattleFrorgGame/Classes/Damage.cs
+++ MonoBattleFrorgGame/Classes/Damage.cs
@@ -6,17 +6,19 @@ using System.Threading.Tasks;
 
 namespace MonoBattleFrorgGame.Classes
 {
-    internal class Damage
+    internal class Damage : ICloneable
     {
         private double amount = 0;
+        public DamageProperties Properties { get; private set; }
+        public object Clone()
+        {
+            var clone = MemberwiseClone() as Damage;
+            clone.Properties = Properties.Clone() as DamageProperties;
+            return clone;
+        }
         public Damage Amount(double count)
         {
             amount = count;
-            return this;
-        }
-        private Damage SplitAmount(double ratio)
-        {
-            amount *= ratio;
             return this;
         }
         internal enum DamageType
@@ -29,12 +31,27 @@ namespace MonoBattleFrorgGame.Classes
             Blast,
             Magic
         }
-        internal class DamageProperties()
+        public IEnumerable<Damage> Split(params double[] ratios)
+        {
+            if (ratios.Sum() <= 0.99 && ratios.Sum() >= 1.01) throw new ArgumentException("Damage splits should total to 1.");
+            IEnumerable<Damage> result = new List<Damage>(ratios.Length);
+            foreach (var i in ratios)
+            {
+                result = result.Append((Clone() as Damage).Amount(amount * i));
+            }
+            return result;
+        }
+        internal class DamageProperties : ICloneable
         {
             private DamageType type = DamageType.None;
             private double critRate = 0;
             private double critDamage = 2;
             private double ignoreDefense = 0;
+            public DamageType GetDamageType() => type;
+            public double GetCritRate() => critRate;
+            public double GetCritDamage() => critDamage;
+            public double GetDefenseIgnore() => ignoreDefense;
+            public object Clone() { return MemberwiseClone(); }
             public DamageProperties @Type(DamageType newType)
             {
                 type = newType;
@@ -56,15 +73,31 @@ namespace MonoBattleFrorgGame.Classes
                 return this;
             }
         }
-        public IEnumerable<Damage> Split(params double[] ratios)
+        internal class DamageModifier
         {
-            if (ratios.Sum() <= 0.99 && ratios.Sum() >= 1.01) throw new ArgumentException("Damage splits should total to 1.");
-            IEnumerable<Damage> result = new List<Damage>(ratios.Length);
-            foreach (var i in ratios)
+            public enum Property
             {
-                result = result.Append(SplitAmount(i));
+                Amount,
+                CritRate,
+                CritDamage,
+                IgnoreDefense
             }
-            return result;
+            public enum Operation
+            {
+                Add,
+                Multiply
+            }
+            public Property damageProperty;
+            public Operation operation;
+            public DamageModifier(Property prop, Operation op)
+            {
+                damageProperty = prop;
+                operation = op;
+            }
+        } // devilish but genius idea. trust
+        public bool Crits()
+        {
+            return 1 < Properties.GetCritRate(); // an RNG is supposed to go here but I haven't made that yet
         }
     }
 }
