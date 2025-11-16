025fca2401abebc62ec8ef3df3faafde37134845
ThatCubicGuy <vlasinalexcatalin@gmail.com>
2025-11-16 23:02:05 +0200

    Minor work on selectable tables. Verdict: make the cell contents mutable, everything else is set in stone.

--- FrogBattle/UI/SelectableTable.cs
+++ FrogBattle/UI/SelectableTable.cs
@@ -16,7 +16,6 @@ namespace FrogBattle.UI
 		private readonly Point cellSize;
 		private readonly Point cellSpacing;
 		private Point selectedItem;
-		private int actualCellCount;
 
 		/// <summary>
 		/// Creates a new selectable table with the given elements.
@@ -28,7 +27,7 @@ namespace FrogBattle.UI
 		/// <param name="cell_size">The bounds within which each cell can draw itself.</param>
 		/// <param name="cell_spacing">The (horizontal, vertical) distance in pixels between each cell rectangle.</param>
 		/// <exception cref="ArgumentOutOfRangeException"></exception>
-		public SelectableTable(ISelectableCell[] cells, ISelectableCell defaultValue, Point grid_size, Point location, Point cell_size, Point cell_spacing = new())
+		public SelectableTable(ISelectableCell[] baseCells, Point grid_size, Point location, Point cell_size, Point cell_spacing = new())
 		{
 			if (grid_size.X <= 0 || grid_size.Y <= 0) throw new ArgumentOutOfRangeException(nameof(grid_size), "Cannot create a menu with less than 1 box!");
             Location = location;
@@ -37,19 +36,9 @@ namespace FrogBattle.UI
 			cellSpacing = cell_spacing;
 			Size = new(cellSize.X * Columns + cellSize.X * (Columns - 1), Rows + cellSize.Y * (Rows - 1));
 			selectedItem = Point.Zero;
-			cellLocations = new Point[BoxCount];
-			this.cells = new ISelectableCell[BoxCount];
-			actualCellCount = cells.Length;
-			Array.Copy(cells, this.cells, actualCellCount);
-			Array.Fill(this.cells, defaultValue, actualCellCount, BoxCount - actualCellCount);
-			for (int i = 0; i < Rows; i++)
-			{
-				for (int j = 0; j < Columns; j++)
-				{
-					// Create a static list of the locations of each box, relative to the table's location.
-					cellLocations[i * Columns + j] = new Point((cellSize.X + cellSpacing.X) * j, (cellSize.Y + cellSpacing.Y) * i);
-				}
-			}
+			cellLocations = new Point[MaximumCellCount];
+			cells = new ISelectableCell[MaximumCellCount];
+			SetCells(baseCells);
         }
 
 		public ISelectableCell Cursor => cells[SelectedItemID];
@@ -61,7 +50,12 @@ namespace FrogBattle.UI
 				selectedItem.Y = value;
 				if (value > Rows - 1) selectedItem.Y = 0;
 				if (value < 0) selectedItem.Y = Rows - 1;
-			}
+
+                if (!IsCellValid(CursorRow, CursorColumn))
+                {
+                    --CursorRow;
+                }
+            }
 		}
 		public int CursorColumn
 		{
@@ -71,6 +65,11 @@ namespace FrogBattle.UI
 				selectedItem.X = value;
 				if (value > Columns - 1) selectedItem.X = 0;
 				if (value < 0) selectedItem.X = Columns - 1;
+
+				if (!IsCellValid(CursorRow, CursorColumn))
+				{
+					--CursorColumn;
+				}
 			}
 		}
 		public int SelectedItemID
@@ -88,7 +87,8 @@ namespace FrogBattle.UI
 		public Point GridSize { get; }
 		public int Rows => GridSize.Y;
 		public int Columns => GridSize.X;
-		public int BoxCount => Rows * Columns;
+		public int MaximumCellCount => Rows * Columns;
+		public int CellCount { get; private set; }
 		public Point Location { get; set; }
 		public Point Size { get; }
 		public Rectangle Bounds => new(Location, Size);
@@ -100,13 +100,31 @@ namespace FrogBattle.UI
 		/// <param name="location">X means columns, Y means rows.</param>
 		/// <returns>An ISelectableCell whose coordinates in this table are equal to <paramref name="location"/>.</returns>
 		public ISelectableCell this[Point location] => this[location.X, location.Y];
+        
+		private bool IsCellValid(int row, int col)
+        {
+            int index = row * Columns + col;
+            return index >= 0 && index < CellCount && cells[index] != null;
+        }
 
-		public void OffsetLocation(int x_offset, int y_offset) => OffsetLocation(new(x_offset, y_offset));
+        public void OffsetLocation(int x_offset, int y_offset) => OffsetLocation(new(x_offset, y_offset));
 		public void OffsetLocation(Point offset) => Location += offset;
         public void MoveCursorLeft() => --CursorColumn;
 		public void MoveCursorRight() => ++CursorColumn;
 		public void MoveCursorUp() => --CursorRow;
 		public void MoveCursorDown() => ++CursorRow;
+
+		public void SetCells(ISelectableCell[] newCells)
+		{
+            CellCount = newCells.Length;
+			Array.Clear(cells);
+			Array.Copy(newCells, cells, CellCount);
+            for (int i = 0; i < CellCount; i++)
+            {
+                // Create a list of the locations of each box, relative to the table's location.
+                cellLocations[i] = new Point((cellSize.X + cellSpacing.X) * (i % Columns), (cellSize.Y + cellSpacing.Y) * (i / Columns));
+            }
+        }
 
 		public void Draw(SpriteBatch spriteBatch)
 		{
